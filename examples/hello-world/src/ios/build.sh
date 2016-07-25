275035ad23255d22d84c64d509975c6e0f4b372e
Ian Katz <iakatz@paypal.com>
2016-07-25 18:00:00 -0400

    [hello world] actually build in build script

--- examples/hello-world/src/ios/build.sh
+++ examples/hello-world/src/ios/build.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
+JS_SRC=node_modules/hello-world/*.js
 set -ex
 bundle install
 npm install --production
-./node_modules/.bin/manticore-gen node_modules/manticore-native/runtime/objc/templates generated/src node_modules/hello-world/*.js
-./node_modules/.bin/mantify generated/manticore_modules.js node_modules/hello-world/*.js
+./node_modules/.bin/manticore-gen node_modules/manticore-native/runtime/objc/templates generated/src $JS_SRC
+./node_modules/.bin/mantify generated/manticore_modules.js $JS_SRC
 pod install
+xcodebuild -workspace "Hello Manticore.xcworkspace" -list
+xcodebuild clean build -workspace "Hello Manticore.xcworkspace" -scheme "Hello Manticore" -sdk iphonesimulator
